062d86d1e0e95a827b105b496dabd5ff813f255c
JackLevenWeb <jackleven.web@gmail.com>
2026-07-15 15:18:45 +0200

    added (WIP) to app description

--- 40kDiceCalc/js/event-manager.js
+++ 40kDiceCalc/js/event-manager.js
@@ -49,7 +49,7 @@ export function initializeWatchers() {
             if (tutType === "main_site") {
                 tutTitle.textContent = "How to use this website";
                 tutBody.innerHTML = `
-                    <p>Welcome to the Munitorum Auspex, a probability calculator for Warhammer 40,000.</p>
+                    <p>Welcome to the Munitorum Auspex(WIP), a probability calculator for Warhammer 40,000.</p>
                     <p style="margin-top: 10px;">This tool calculates exact probabilities for each phase of the game between attacking and defending units. To ensure pinpoint accuracy, this engine runs <strong>100,000 Monte Carlo simulations</strong> per unit, per modifier, per phase.</p>
                     <p style="margin-top: 10px;">Look for the <strong>?</strong> icons throughout the app to learn how each specific section functions.</p><br>
                     <p style="margin-top: 10px;">This app is a personal project created for learning and experimentation in 40K. It’s inspired by tools like Tactical Cogitator, but it’s not commercial. I built it as a portfolio piece to showcase my work on GitHub. <a href="https://github.com/JackLevenWeb" target="_blank" style="color: var(--theme-accent);">https://github.com/JackLevenWeb</a></p>
