481ae59e5c3220b5c213d8904515875927527b7b
JackLevenWeb <jackleven.web@gmail.com>
2026-08-25 22:26:14 +0200

    removed old ID

--- 40kDiceCalc/js/auth-manager.js
+++ 40kDiceCalc/js/auth-manager.js
@@ -2,7 +2,7 @@
 
 const msalConfig = {
     auth: {
-        clientId: "4df1096c-4fe4-4d67-b93e-043e68f12ec0",
+        clientId: "",
         authority: "https://login.microsoftonline.com/common",
         redirectUri: window.location.origin + window.location.pathname
     },
